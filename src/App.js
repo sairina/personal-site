215d5c5ed6311c5b03ef5f49f8cd696e37c8bb95
sairina <sairina.merinotsui@gmail.com>
2020-03-20 20:29:05 -0700

    imported bootstrap and react-bootstrap into project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from './NavBar';
 import Home from './Home';
+import AboutMe from './AboutMe';
+import Projects from './Projects';
 
 function App() {
   return (
@@ -12,6 +15,12 @@ function App() {
           <NavBar />
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/about-me">
+            <AboutMe />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
           </Route>
         </Switch>
       </BrowserRouter>
