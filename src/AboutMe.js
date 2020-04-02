203b27a8ad0fbda6cdfa225e107f6479ced30cbb
sairina <sairina.merinotsui@gmail.com>
2020-04-01 22:44:54 -0700

    added about me content and css

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './AboutMe.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function AboutMe() {
   return (
-    <div>
-      <h1>About Me</h1>
-    </div>
+    <Container fluid className="AboutMe">
+      <Row className="justify-content-md-center">
+        <Col md="auto"><h1>About Me</h1></Col>
+      </Row>
+      <Row>
+        <Col md="auto"><p>
+        I graduated from NYU with a BA in Anthropology and Pre-med, but joined Teach for America as a high school chemistry teacher to work on fixing the disparities in urban education.
+        I earned my MS in Science Education, and have been working in various roles in education, from classroom teacher, assistant principal, director of science content at TFA, and assistant principal.
+        Somewhere in there, my team won the EDUStartUp Weekend. nbd.
+        </p></Col>
+      </Row>
+    </Container >
   );
 }
 
