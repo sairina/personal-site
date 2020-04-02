b04b67efa11081dc17465a85b7e956077fc4078a
sairina <sairina.merinotsui@gmail.com>
2020-04-01 22:20:57 -0700

    updated routes so that transition occurs

--- src/Routes.js
+++ src/Routes.js
@@ -1,35 +1,41 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './Routes.css';
 import { Route, Switch, withRouter } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Home from './Home';
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Resume from './Resume';
+import Contact from './Contact';
 
 function Routes({ location }) {
   return (
     <div className="Routes">
-      <TransitionGroup>
+      <TransitionGroup className="Routes-transition-group">
         <CSSTransition
           key={location.key}
           timeout={{ enter: 300, exit: 300 }}
+          classNames={'fade'}
         >
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/about-me">
-              <AboutMe />
-            </Route>
-            <Route exact path="/resume">
-              <Resume />
-            </Route>
-            <Route exact path="/projects">
-              <Projects />
-            </Route>
-          </Switch>
+          <section className="Routes-section">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about-me">
+                <AboutMe />
+              </Route>
+              <Route exact path="/contact">
+                <Contact />
+              </Route>
+              <Route exact path="/resume">
+                <Resume />
+              </Route>
+              <Route exact path="/projects">
+                <Projects />
+              </Route>
+            </Switch>
+          </section>
         </CSSTransition>
       </TransitionGroup>
     </div>
