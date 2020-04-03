72ef76317dd6141cb0fa69240daa6d5713f679ec
sairina <sairina.merinotsui@gmail.com>
2020-04-02 23:04:00 -0700

    added animatedswitch and react-pose for transitions

--- src/Routes.js
+++ src/Routes.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import './Routes.css';
-import { Route, Switch, withRouter } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Route, withRouter } from "react-router-dom";
+import { AnimatedSwitch } from "./AnimatedSwitch";
 import Home from './Home';
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Resume from './Resume';
 import Contact from './Contact';
 
-function Routes({ location }) {
+const Routes = withRouter(({ location }) => {
   return (
     <div className="Routes">
-      <TransitionGroup className="Routes-transition-group">
-        <CSSTransition
-          key={location.key}
-          timeout={{ enter: 300, exit: 300 }}
-          classNames={'fade'}
-        >
-          <section className="Routes-section">
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/about-me">
-                <AboutMe />
-              </Route>
-              <Route exact path="/contact">
-                <Contact />
-              </Route>
-              <Route exact path="/resume">
-                <Resume />
-              </Route>
-              <Route exact path="/projects">
-                <Projects />
-              </Route>
-            </Switch>
-          </section>
-        </CSSTransition>
-      </TransitionGroup>
+      <section className="Routes-section">
+        <AnimatedSwitch location={location}>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about-me">
+            <AboutMe />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/resume">
+            <Resume />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+        </AnimatedSwitch>
+      </section>
     </div>
   )
-}
+});
 
-export default withRouter(Routes);
+export default Routes;
