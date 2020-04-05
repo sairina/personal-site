4519b100372b96b1769ef5e81cfb723719d134bc
sairina <sairina.merinotsui@gmail.com>
2020-04-04 23:36:44 -0700

    updated resume and css for resume component

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import './Resume.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-function Resume(){
+function Resume() {
   return (
-    <>
-      Resume
-    </>
+    <Container fluid className="Resume">
+      <Row className="justify-content-md-center">
+        <Col md="auto"><h1>Resume</h1></Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <div class="">
+
+            <ListGroup variant="flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+              <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            </ListGroup>
+
+            <h3>Education</h3>
+            <h4>Full Stack Software Engineering | 2020</h4>
+            <h5>Rithm School</h5>
+            <p>Edie Windsor Scholar</p>
+          </div>
+        </Col>
+        <Col md={6}>
+          <h3>Experience</h3>
+        </Col>
+      </Row>
+    </Container >
   );
 }
 
