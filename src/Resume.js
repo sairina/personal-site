d91b3e0437a086a05895552da563997d3420f5a1
sairina <sairina.merinotsui@gmail.com>
2020-04-04 23:57:06 -0700

    removed listgroup from component

--- src/Resume.js
+++ src/Resume.js
@@ -3,7 +3,6 @@ import './Resume.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
 
 function Resume() {
   return (
@@ -13,18 +12,8 @@ function Resume() {
       </Row>
       <Row>
         <Col md={6}>
-          <div>
-            <ListGroup className="Resume-ListGroup" variant="flush">
-              <ListGroup.Item><h3>Education</h3>
-                <h4>Full Stack Software Engineering | 2020</h4>
-                <h5>Rithm School</h5>
-                <p>Edie Windsor Scholar</p></ListGroup.Item>
-              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-              <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-              <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            </ListGroup>
-
-            <h3>Education</h3>
+          <h3>Education</h3>
+          <div className="Resume-Education-Item">
             <h4>Full Stack Software Engineering | 2020</h4>
             <h5>Rithm School</h5>
             <p>Edie Windsor Scholar</p>
