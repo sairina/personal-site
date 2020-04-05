b86720dcd7232a2d5ecd02b5ee8a0dfb1da51c2d
sairina <sairina.merinotsui@gmail.com>
2020-04-05 00:29:50 -0700

    updated navbar order for contact and resume

--- src/NavBar.js
+++ src/NavBar.js
@@ -14,8 +14,8 @@ function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link as={Link} to="/about-me">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
             <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
 
             <NavDropdown title="Portfolio" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
