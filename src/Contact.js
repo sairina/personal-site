fd83fa2d9e6983c363d2f77162fe6e638b2b5270
sairina <sairina.merinotsui@gmail.com>
2020-04-06 23:16:12 -0700

    consolidated css in App as parent

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Contact.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-function Contact(){
+function Contact() {
   return (
-
-      <h1>Contact Me</h1>
+    <Container fluid className="Contact">
+      <Row className="justify-content-md-center">
+        <Col>
+          <h1>Get in Touch</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col className="Contact-Col">
+        <h2>Message Me</h2>
+        </Col>
+        <Col className="Contact-Col">
+          <h2>Contact Info</h2>
+          <p>Always available for freelance work if the right project comes along, Feel free to contact me!</p>
+        </Col>
+      </Row>
+    </Container >
   );
 }
 
