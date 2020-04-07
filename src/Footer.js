28f492db88c06560aca637428dc746f27f256ec5
sairina <sairina.merinotsui@gmail.com>
2020-04-06 23:02:53 -0700

    updated links in footer to open in new tabs

--- src/Footer.js
+++ src/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
   return (
     <div className="Footer">
       <div className="Footer-container">
-        <a href="https://github.com/sairina"><FontAwesomeIcon className="Footer-links" icon={['fab', 'github']}/></a>
-        <a href="https://www.linkedin.com/in/sairina"><FontAwesomeIcon className="Footer-links" icon={['fab', 'linkedin']}/></a>
-        <a href="mailto:sairina.merinotsui@gmail.com"><FontAwesomeIcon className="Footer-links" icon={['fas', 'envelope-square']}/></a>
+        <a href="https://github.com/sairina" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon className="Footer-links" icon={['fab', 'github']}/></a>
+        <a href="https://www.linkedin.com/in/sairina" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon className="Footer-links" icon={['fab', 'linkedin']}/></a>
+        <a href="mailto:sairina.merinotsui@gmail.com" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon className="Footer-links" icon={['fas', 'envelope-square']}/></a>
       </div>
     </div>
   );
