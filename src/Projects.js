cb731d8359377b7f9d231bc57f18edf296145371
sairina <sairina.merinotsui@gmail.com>
2020-04-14 22:52:13 -0700

    added project component into site

--- src/Projects.js
+++ src/Projects.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Projects.css';
 
 function Projects(){
   return (
-    <>
-      PROJECTS
-    </>
+    <Container fluid className="Projects">
+      <Row className="justify-content-md-center">
+        <Col>
+          <h1>Projects</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
