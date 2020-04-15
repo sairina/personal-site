d1eb3b24ad00ea440e6f2bd1bae045c47da03944
sairina <sairina.merinotsui@gmail.com>
2020-04-14 22:41:17 -0700

    added deployed warbler to navbar link, added project page

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,7 @@ import './NavBar.css';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
+
 function NavBar() {
   return (
     <div>
@@ -17,11 +18,10 @@ function NavBar() {
             <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
 
             <NavDropdown title="Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/projects">All projects</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item target="_blank" href="https://warbler-sairina.herokuapp.com/">Warbler</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Microblog</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
