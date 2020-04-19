a2eb02f0193a92e7bbb8cced9f9e6fe9f89350d4
sairina <sairina.merinotsui@gmail.com>
2020-04-18 20:25:38 -0700

    updated mobile responsiveness for vh and h1 styling

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,7 @@ function Home() {
   return (
     <Container fluid className="Home">
       <Row className="justify-content-md-center">
-        <Col></Col>
         <Col md="auto"><h1>Sairina Merino Tsui</h1></Col>
-        <Col></Col>
         <Col md="auto"><h5>Seeking to use technology to elevate the voices and perspectives of marginalized communities.</h5></Col>
       </Row>
     </Container>
