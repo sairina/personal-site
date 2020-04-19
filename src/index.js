9a1d899a7ac317773524bab5d19319cbf21c2777
sairina <sairina.merinotsui@gmail.com>
2020-04-18 20:17:15 -0700

    removed React strict mode

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ WebFont.load({
 });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
