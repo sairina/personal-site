1f5b57efef1cb639adeb9cb25ed5d14bf89c6467
sairina <sairina.merinotsui@gmail.com>
2020-08-01 12:48:07 -0700

    added comment - pair programming badge

--- src/Footer.js
+++ src/Footer.js
@@ -9,6 +9,10 @@ function Footer() {
         <a href="https://github.com/sairina" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon className="Footer-links" icon={['fab', 'github']}/></a>
         <a href="https://www.linkedin.com/in/sairina" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon className="Footer-links" icon={['fab', 'linkedin']}/></a>
         <a href="mailto:sairina.merinotsui@gmail.com" target="_blank" rel="noreferrer noopener"><FontAwesomeIcon className="Footer-links" icon={['fas', 'envelope-square']}/></a>
+      {/* <a href="mailto:sairina.merinotsui@gmail.com?subject=Pair%20program%20with%20me" title="Pair program with me!">
+  <img  src="http://pairprogramwith.me/badge.png"
+        alt="Pair program with me!" />
+</a> */}
       </div>
     </div>
   );
