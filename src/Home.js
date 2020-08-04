4273c2442373d73099f7f388e99d4b0f24a848e3
sairina <sairina.merinotsui@gmail.com>
2020-08-03 20:20:31 -0700

    updated background for home

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,16 @@ import './Home.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+// import yellowPeril from '../src/images/web_goto_720x_copy.png';
 
 function Home() {
   return (
-    <Container fluid className="Home">
-      <Row className="justify-content-md-center">
-        <Col md="auto"><h1>Sairina Merino Tsui</h1></Col>
-        <Col md="auto"><h5>Using technology to elevate the voices and perspectives of marginalized communities.</h5></Col>
-      </Row>
-    </Container>
+      <Container fluid className="Home">
+        <Row className="justify-content-md-center boxy">
+          <Col md="auto"><h1>Sairina Merino Tsui</h1></Col>
+          <Col md="auto"><h5>Seeking to use software engineering to elevate the voices of marginalized communities.</h5></Col>
+        </Row>
+      </Container>
   );
 }
 
